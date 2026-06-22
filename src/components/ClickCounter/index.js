ce5b05bc23c9ebf4f3d51d4f922d7ba45f041309
ravikumarpalla <ravikumarpalla000@gmail.com>
2026-06-22 16:27:12 +0000

    updated code

---
+++ src/components/ClickCounter/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+import {Component} from 'react'
+
+class ClickCounter extends Component {
+  state = {count: 0}
+
+  onIncrement = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div className="container">
+        <h1>
+          The Button has been clicked <span>{count}</span> times
+        </h1>
+        <p>Click the button to increase the count</p>
+        <button onClick={this.onIncrement}>Click Me!</button>
+      </div>
+    )
+  }
+}
+
+export default ClickCounter
